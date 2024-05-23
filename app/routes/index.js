49ba559de9081efe28e7a613e5de404071eebf28
ryzen500 <ahmadtsani145@gmail.com>
2024-05-23 13:28:32 +0700

    Fixed CORS

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,29 +1,29 @@
-const express = require("express");
-const { messageRoute } = require("./reservation.routes");
+const express = require('express');
+const { messageRoute } = require('./reservation.routes');
 
-// const { todoRoute } = require("./todo.route");
-// const { authRoute } = require("./auth.route");
 const router = express.Router();
 
 const setCorsHeaders = (req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*'); // Allow all origins
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Allow specific methods
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allow specific headers
-    next();
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Allow specific methods
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Allow specific headers
+    
+    // Handle preflight requests
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(204); // No Content
+    } else {
+        next();
+    }
 };
 
 router.use(setCorsHeaders); // Apply CORS headers to all routes
 
-
-router.get("/", (req, res) => {
-
-  res.json("This is simple todolist api");
+router.get('/', (req, res) => {
+    res.json('This is a simple todolist API');
 });
 
-router.use("/reservation", messageRoute);
-// router.use("/todo", todoRoute);
-// router.use("/auth", authRoute);
+router.use('/reservation', messageRoute);
 
 module.exports = {
-  allRouter: router,
+    allRouter: router,
 };
